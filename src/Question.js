fd0cbebe7f92dd450435f2fac0d95fb49f42e647
Uddesh Pujari <uddesh.pujari123@gmail.com>
2024-04-25 23:53:26 +0530

    employee add done

---
+++ src/Question.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const PrevBTN = ({onClick})=>{
+  return (
+    <div className='pre'>
+      <button 
+        onClick={()=>{
+          onClick()
+        }}
+      >
+        Prev
+      </button>
+    </div>
+  )
+}
+
+const NextBTN = ({onClick})=>{
+  return (
+    <div className='next'>
+      <button
+        onClick={()=>{
+          onClick()
+        }}
+      >
+        Next
+      </button>
+    </div>
+  )
+}
+
+const SubmitBTN = ({onClick})=>{
+
+  return (
+    <div>
+      <button
+        onClick={()=>{
+          onClick()
+        }}
+      >Submit QUIZ</button>
+    </div>
+  )
+
+}
+
+const Question = ({questionData, setQuestionData, currentQuestionIndex, setCurrentQuestionIndex}) => {
+  return (
+    <div>
+        <h4>Question{questionData[currentQuestionIndex].question_no} : {questionData[currentQuestionIndex].question}</h4>
+        {questionData[currentQuestionIndex]?.options?.map(option=><div><button>{option}</button></div>)}
+        {currentQuestionIndex!=0 && 
+        <PrevBTN onClick={()=>{
+          setCurrentQuestionIndex(prevValue=>prevValue-1)
+        }}/>}
+        {currentQuestionIndex==questionData.length-1 ? <SubmitBTN onClick={()=>{}}/> : <NextBTN onClick={()=>{setCurrentQuestionIndex(prevValue=>prevValue+1)}}/> }
+    </div>
+  )
+}
+
+export default Question
